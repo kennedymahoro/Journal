d1f00afa51901d74f2ca309c0bb39b66c6379eb2
Kennedy Mahoro <kennedymahoro1@gmail.com>
2022-06-30 15:44:01 +0000

    querselected all of the html elements

---
+++ index.ts
@@ -0,0 +1,7 @@
+const Nav:HTMLElement = document.querySelector('nav') as HTMLElement;
+const Input_title:HTMLInputElement = document.querySelector('data-main-input-title') as HTMLInputElement;
+const Display_time:HTMLHeadElement = document.querySelector('data-display-time') as HTMLHeadElement;
+const Save_btn:HTMLButtonElement = document.querySelector('data-save-btn') as HTMLButtonElement;
+const New_btn:HTMLButtonElement = document.querySelector('data-new-btn') as HTMLButtonElement;
+const Delete_btn:HTMLButtonElement = document.querySelector('data-delete-btn') as HTMLButtonElement;
+const Textarea:HTMLTextAreaElement = document.querySelector('data-textarea') as HTMLTextAreaElement;
