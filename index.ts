789e9bc7ed9b1250c4e6aef45c13a9f71248f8a2
Kennedy Mahoro <kennedymahoro1@gmail.com>
2022-07-04 15:12:16 +0000

    some issues

--- index.ts
+++ index.ts
@@ -5,3 +5,17 @@ const Save_btn:HTMLButtonElement = document.querySelector('data-save-btn') as HT
 const New_btn:HTMLButtonElement = document.querySelector('data-new-btn') as HTMLButtonElement;
 const Delete_btn:HTMLButtonElement = document.querySelector('data-delete-btn') as HTMLButtonElement;
 const Textarea:HTMLTextAreaElement = document.querySelector('data-textarea') as HTMLTextAreaElement;
+
+class NewBtn{
+    this.date:string
+    constructor(title:string,date:string,text:string){
+
+    }
+    getID(){
+        return this.date;
+    }
+}
+
+Save_btn.addEventListener('click', ()=>void{
+
+})
