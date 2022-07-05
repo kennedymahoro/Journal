1519089f18a040e7e6b76448d5ca13060a5619af
Kennedy Mahoro <kennedymahoro1@gmail.com>
2022-07-05 20:08:25 +0000

    update

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 "use strict";
+const Nav = document.querySelector('nav');
+const Input_title = document.querySelector('data-main-input-title');
+const Display_time = document.querySelector('data-display-time');
+const Save_btn = document.querySelector('data-save-btn');
+const New_btn = document.querySelector('data-new-btn');
+const Delete_btn = document.querySelector('data-delete-btn');
+const Textarea = document.querySelector('data-textarea');
+class NewButton {
+    constructor(text, title) {
+        this.Id = Date.now().toString();
+        this.text = text;
+        this.title = title;
+    }
+    getId() {
+        return this.Id;
+    }
+    createButton() {
+        const Button = document.createElement('button');
+        Button.setAttribute('id', this.Id);
+        Button.setAttribute('class', 'nav-btn');
+        return Button;
+    }
+}
+Save_btn.addEventListener('click', () => void {});
